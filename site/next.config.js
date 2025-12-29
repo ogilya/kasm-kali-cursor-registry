67dec246799be4d6b8eec48f0b2cc5a3d6bd6362
ogilya <ogilya@users.noreply.github.com>
2025-12-29 20:03:34 +0000

    Update config for custom registry

--- site/next.config.js
+++ site/next.config.js
@@ -7,11 +7,11 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://ogilya.github.io/kasm-kali-cursor-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-kali-cursor-registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
